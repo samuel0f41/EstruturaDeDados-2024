3b40403ab8516ab9ce3da609dd6ce4aa85c28c58
Samuel Felipe <126738365+samuel0f41@users.noreply.github.com>
2024-03-21 23:05:25 -0300

    Delete Quiz directory

--- Quiz/src/main/java/br/ufpb/samuel/SistemaQuiz.java
+++
@@ -1,9 +0,0 @@
-package br.ufpb.samuel;
-import java.util.List;
-public interface SistemaQuiz {
-    void cadastraPergunta (Pergunta p);
-    Pergunta sorteiaPergunta();
-    List<Pergunta> todasAsPerguntas();
-
-
-}
